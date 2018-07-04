a0d8c0b0e6ea3692d2e63c3d0a043f9e02f10e17
Sanushen Govender <sanushen@gmail.com>
2018-07-04 22:41:39 +0200

    Added some routes

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+
+class Router
+{
+
+    /**
+     * Associative array of routes (the routing table)
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * Add a route to the routing table
+     *
+     * @param string $route  The route URL
+     * @param array  $params Parameters (controller, action, etc.)
+     *
+     * @return void
+     */
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * Get all the routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+}
