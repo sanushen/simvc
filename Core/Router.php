f335430c44fbbadc3c07ea780ba0e80a1e936b48
Sanushen Govender <sanushen@gmail.com>
2018-07-05 18:35:19 +0200

    Wrong file

--- Core/Router.php
+++ Core/Router.php
@@ -3,4 +3,28 @@
 class Router
 {
 
+    /**
+     * Routing table
+     * @var array
+     */
+    protected $routes = [];
+
+    /**
+     * @param string $route  URL
+     * @param array  $params
+     *
+     * @return void
+     */
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 }
