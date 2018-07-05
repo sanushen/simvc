8b09177e5a1762b2a67e5b0747aff124d7279129
Sanushen Govender <sanushen@gmail.com>
2018-07-05 18:40:32 +0200

    Added some routes

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
 
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->add('', ['controller' => 'Homepage', 'action' => 'index']);
+$router->add('blog',['controller' => 'Blog', 'action' => 'index']);
+
+$url = $_SERVER['QUERY_STRING'];
