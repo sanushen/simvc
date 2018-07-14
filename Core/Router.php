76f653309dc4f06cf2ca201380b03bec4220fca5
Sanushen Govender <sanushen@gmail.com>
2018-07-15 00:56:54 +0200

    getNamespace method

--- Core/Router.php
+++ Core/Router.php
@@ -84,7 +84,7 @@ class Router
         if ($this->matchRoute($url)) {
             $controller = $this->params['controller'];
             $controller = $this->convertToStudlyCaps($controller);
-            $controller = "App\Controllers\\$controller";
+            $controller = $this->getNamespace().$controller;
 
             if (class_exists($controller)) {
                 $controller_object = new $controller($this->params);
@@ -122,6 +122,20 @@ class Router
     protected function convertToCamelCase($string)
     {
         return lcfirst($this->convertToStudlyCaps($string));
+    }
+
+    /**
+     * @return string The request URL
+     */
+    protected function getNamespace()
+    {
+        $namespace = 'App\Controllers\\';
+
+        if (array_key_exists('namespace', $this->params)) {
+            $namespace .= $this->params['namespace'] . '\\';
+        }
+
+        return $namespace;
     }
 
 }
