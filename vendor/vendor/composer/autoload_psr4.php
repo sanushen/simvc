db1b52a5e483cdbfdcb012bb43c6282de8efa4f2
Sanushen Govender <sanushen@gmail.com>
2018-07-16 14:55:44 +0200

    Moving autoloader to composer - needs debugging

--- vendor/vendor/composer/autoload_psr4.php
+++ vendor/vendor/composer/autoload_psr4.php
@@ -9,4 +9,6 @@ return array(
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Core\\' => array($baseDir . '/Core'),
+    'App\\' => array($baseDir . '/App'),
 );
--- vendor/vendor/composer/autoload_static.php
+++ vendor/vendor/composer/autoload_static.php
@@ -21,6 +21,14 @@ class ComposerStaticInit54e38a3977a804ebf43eb671138dfa81
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
+        'C' => 
+        array (
+            'Core\\' => 5,
+        ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -35,6 +43,14 @@ class ComposerStaticInit54e38a3977a804ebf43eb671138dfa81
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'Core\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/Core',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/App',
         ),
     );
 
