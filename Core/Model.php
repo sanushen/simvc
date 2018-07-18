75f47f1dec8f53187750757360175ba32434e55b
Sanushen Govender <sanushen@gmail.com>
2018-07-18 17:13:23 +0200

    Config updates

--- Core/Model.php
+++ Core/Model.php
@@ -3,6 +3,7 @@
 namespace Core;
 
 use PDO;
+use App\Config;
 
 abstract class Model
 {
@@ -14,14 +15,10 @@ abstract class Model
         static $db = null;
 
         if ($db === null) {
-            $host = 'localhost';
-            $dbname = 'simvc';
-            $username = 'user';
-            $password = 'secret';
 
             try {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
-                    $username, $password);
+                $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
 
             } catch (\PDOException $e) {
                 echo $e->getMessage();
