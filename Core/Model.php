9886956cac549bc6c54636645b8ea28612383efe
Sanushen Govender <sanushen@gmail.com>
2018-07-18 17:07:23 +0200

    DB connection class

--- Core/Model.php
+++ Core/Model.php
@@ -6,5 +6,28 @@ use PDO;
 
 abstract class Model
 {
-    
+    /**
+     * @return mixed
+     */
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $host = 'localhost';
+            $dbname = 'simvc';
+            $username = 'user';
+            $password = 'secret';
+
+            try {
+                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+                    $username, $password);
+
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        return $db;
+    }
 }
